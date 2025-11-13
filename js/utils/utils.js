47c02f9c77a7de6ce1be58e80a66933cfcf77bcf
Felipe Bonilha <bonilhfe@amazon.com>
2025-11-13 20:10:25 -0300

    Publish site: deploy src to gh-pages

---
+++ js/utils/utils.js
@@ -0,0 +1,15 @@
+const Utils = ( function () {
+
+    async function hashPassword(password) {
+        const enc = new TextEncoder();
+        const data = enc.encode(password);
+        const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    }
+
+    return {
+        hashPassword,
+    };
+})();    
+    
